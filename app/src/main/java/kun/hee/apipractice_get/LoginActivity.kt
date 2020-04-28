a7a408fb6dc50d777dae1e1a9901d09f463cab7d
smlrkh1107 <krae7789230@gmail.com>
2020-04-28 19:45:08 +0900

    로그인화면 xml

--- app/src/main/java/kun/hee/apipractice_get/LoginActivity.kt
+++ app/src/main/java/kun/hee/apipractice_get/LoginActivity.kt
@@ -1,12 +1,11 @@
 package kun.hee.apipractice_get
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LogInActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
     }
