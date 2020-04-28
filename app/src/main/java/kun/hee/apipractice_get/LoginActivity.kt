ac44e92b313ee220ab9e22f2cf82e037f6040868
smlrkh1107 <krae7789230@gmail.com>
2020-04-28 20:04:32 +0900

    서버 연결 기능 추가 - 로그인 응답 - 인터넷연결

--- app/src/main/java/kun/hee/apipractice_get/LoginActivity.kt
+++ app/src/main/java/kun/hee/apipractice_get/LoginActivity.kt
@@ -1,7 +1,10 @@
 package kun.hee.apipractice_get
 
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
+import kun.hee.apipractice_get.utils.ConnectServer
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,15 @@ class LoginActivity : BaseActivity() {
 
     override fun setupEvents() {
         loginBtn.setOnClickListener {
+            val id = idEdt.text.toString()
+            val pw = pwEdt.text.toString()
 
+            ConnectServer.postRequestLogin(mContext, id, pw, object: ConnectServer.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    Log.d("로그인응답", json.toString())
+                }
+
+            })
         }
 
     }
