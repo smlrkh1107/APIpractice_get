f97406528cd643d84269afbc0b29a77a946cdec1
smlrkh1107 <krae7789230@gmail.com>
2020-04-28 19:51:42 +0900

    splash 액티비티 화면 구현 [넘어가기, 제목바없애기]

--- app/src/main/java/kun/hee/apipractice_get/SplashActivity.kt
+++ app/src/main/java/kun/hee/apipractice_get/SplashActivity.kt
@@ -1,7 +1,9 @@
 package kun.hee.apipractice_get
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,12 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//  1.5초동안 로고를 보여준 뒤, 로그인 액티비티로 전환
+        Handler().postDelayed({
+            val myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+            finish() // 뒤로가기해도 이 화면으로 가지지 않아.
+        },1500)
 
     }
 
